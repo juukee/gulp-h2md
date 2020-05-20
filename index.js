0a2b1f18b0f6f3292e55e109c7cc7adc71d731e3
Juukee <admin@juukee.cn>
2020-05-21 00:20:12 +0800

    更改处理依赖

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
 const chalk = require('chalk');
 const PluginError = require('plugin-error');
 const replaceExtension = require('replace-ext');
-const stripAnsi = require('strip-ansi');
 const through = require('through2');
-const clonedeep = require('lodash/cloneDeep');
 const path = require('path');
-const html2md = require('html-to-md');
 const log = require('fancy-log');
-
+const TurndownService = require('turndown');
 const PLUGIN_NAME = 'gulp-h2md';
 
-//////////////////////////////
-// Main Gulp H2md function
-//////////////////////////////
-const gulpH2md = (options, sync) => through.obj((file, enc, cb) => { // eslint-disable-line consistent-return
-
+const gulpH2md = (options, sync) => through.obj(async (file, enc, cb) => { // eslint-disable-line consistent-return
+    let opts = Object.assign({}, options || {});
     const validExtensions = ['.html', '.htm'];
+    const errorM = (error) => {
+        const filePath = (error.file === 'stdin' ? file.path : error.file) || file.path;
+        const relativePath = path.relative(process.cwd(), filePath);
+        const message = [chalk.underline(relativePath), error.formatted].join('\n');
+        error.messageFormatted = message; // eslint-disable-line no-param-reassign
+        error.messageOriginal = error.message; // eslint-disable-line no-param-reassign
+        error.message = stripAnsi(message); // eslint-disable-line no-param-reassign
+        error.relativePath = relativePath; // eslint-disable-line no-param-reassign
+        return cb(new PluginError(PLUGIN_NAME, error));
+    };
 
-    if (file.isNull()) {
+    function logM(msg,status='error') {
+        if(status=='error'){
+            log(`${PLUGIN_NAME}: ${chalk.red(msg)} ${chalk.blue(file.relative)}`);
+            exit();
+        }else{
+            log(`${PLUGIN_NAME}: ${chalk.green(msg)} ${chalk.blue(file.relative)}`);
+        }
+    }
+
+    if (file.isNull()){
         log(`${PLUGIN_NAME}: ${chalk.red('File length is null')} ${chalk.blue(file.relative)}`);
         return cb();
     }
@@ -26,14 +39,11 @@ const gulpH2md = (options, sync) => through.obj((file, enc, cb) => { // eslint-d
         log(`${PLUGIN_NAME}: ${chalk.red('Steam file is unsupported ')} ${chalk.blue(file.relative)}`);
         return cb();
     }
-
     if (path.basename(file.path).indexOf('_') === 0) {
         log(`${PLUGIN_NAME}: ${chalk.red('File length is 0 bytes')} ${chalk.blue(file.relative)}`);
         return cb();
     }
-
-    if (!file.contents.length) {
-        file.path = replaceExtension(file.path, '.md'); // eslint-disable-line no-param-reassign
+    if (!file.contents.length){
         log(`${PLUGIN_NAME}: ${chalk.red('File length is 0 bytes')} ${chalk.blue(file.relative)}`);
         return cb();
     }
@@ -41,69 +51,34 @@ const gulpH2md = (options, sync) => through.obj((file, enc, cb) => { // eslint-d
         log(`${PLUGIN_NAME}: ${chalk.red('Skipping unsupported html file')} ${chalk.blue(file.relative)}`);
         return cb();
     }
-
-    const opts = clonedeep(options || {});
-    opts.data = file.contents.toString();
-
-    // we set the file path here so that libhtml can correctly resolve import paths
-    opts.file = file.path;
-
     
-    //////////////////////////////
-    // Handles error message
-    //////////////////////////////
-    const errorM = (error) => {
-        const filePath = (error.file === 'stdin' ? file.path : error.file) || file.path;
-        const relativePath = path.relative(process.cwd(), filePath);
-        const message = [chalk.underline(relativePath), error.formatted].join('\n');
-
-        error.messageFormatted = message; // eslint-disable-line no-param-reassign
-        error.messageOriginal = error.message; // eslint-disable-line no-param-reassign
-        error.message = stripAnsi(message); // eslint-disable-line no-param-reassign
-        error.relativePath = relativePath; // eslint-disable-line no-param-reassign
-
-        return cb(new PluginError(PLUGIN_NAME, error));
-    };
-    
+   
     if (sync !== true) {
-        //////////////////////////////
-        // Async H2md render
-        //////////////////////////////
-        const callback = (error, obj) => { // eslint-disable-line consistent-return
-            if (error) {
-              return errorM(error);
-            }
-            return cb(null,obj);
-          };
-
         (async () => {
             try {
-                const data = await html2md(file.contents.toString(enc), opts);
-                file.contents = Buffer.from(data);
+                const data = await new TurndownService().turndown(file.contents.toString(), options);
+                file.contents = Buffer.allocUnsafe && Buffer.from(data) ? Buffer.from(data) : new Buffer(data);
                 file.path = replaceExtension(file.path, '.md'); 
-                log(`${PLUGIN_NAME}:`, chalk.green('✔ ') + file.relative + chalk.green(' Async Converted!'));
-                callback(null, file);
+               
+                log(`${PLUGIN_NAME}:`, chalk.green('✔ ') + file.relative + chalk.blue(` Async Converted!`));
             } catch (error) {
-                callback(new PluginError(PLUGIN_NAME, error, { fileName: file.path }));
-                log(`${PLUGIN_NAME}:`, chalk.yellow('X ') + file.relative + chalk.red(`Async Failed!`));
-                return cb();
+                cb(new PluginError(PLUGIN_NAME, error, { fileName: file.path }));
+                    log(`${PLUGIN_NAME}:`, chalk.yellow('X ') + file.relative + chalk.red(`Async Failed!`));
+                    return cb();
             }
         })();
 
 
     } else {
-        //////////////////////////////
-        // Sync H2md render
-        //////////////////////////////
         try {
-            const data = html2md(file.contents.toString(enc), opts);
-            file.contents = Buffer.from(data);
+            const data = new TurndownService().turndown(file.contents.toString(), options);
+            file.contents = Buffer.allocUnsafe && Buffer.from(data) ? Buffer.from(data) : new Buffer(data);
             file.path = replaceExtension(file.path, '.md'); 
-            callback(null, file);
-            log(`${PLUGIN_NAME}:`, chalk.green('✔ ') + file.relative + chalk.gray(`Sync Converted!`));
+            cb(null, file);
+            log(`${PLUGIN_NAME}:`, chalk.green('✔ ') + file.relative + chalk.blue(` Sync Converted!`));
         } catch (error) {
-            callback(new PluginError(PLUGIN_NAME, error, { fileName: file.path }));
-                log(`${PLUGIN_NAME}:`, chalk.yellow('X ') + file.relative + chalk.red(`Async Failed!`));
+            cb(new PluginError(PLUGIN_NAME, error, { fileName: file.path }));
+                log(`${PLUGIN_NAME}:`, chalk.yellow('X ') + file.relative + chalk.red(` Sync Failed!`));
                 return cb();
         }
     }
@@ -112,9 +87,6 @@ const gulpH2md = (options, sync) => through.obj((file, enc, cb) => { // eslint-d
 
 gulpH2md.sync = options => gulpH2md(options, true);
 
-//////////////////////////////
-// Log errors nicely
-//////////////////////////////
 gulpH2md.logError = function logError(error) {
     const message = new PluginError(PLUGIN_NAME, error.messageFormatted).toString();
     process.stderr.write(`${message}\n`);
